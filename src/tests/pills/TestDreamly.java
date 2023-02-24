1a711e225e2b1fac2133c8616bd110fd9420c7d2
axsomer <alxsmr99@gmail.com>
2023-02-24 14:19:42 -0500

    added inital tests

---
+++ src/tests/pills/TestDreamly.java
@@ -0,0 +1,94 @@
+package tests.pills;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import pills.Dreamly;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;	
+
+public class TestDreamly {
+    private final String CORRECT_NAME = "Dreamly";
+    private final double TEST_STRENGTH = 1.1;
+    private final double TEST_SIZE = 2.2;
+    private final String TEST_COLOR = "White";
+    private final String TEST_CASING = "X";
+    private final String TEST_SOLUTION = "Y";
+    private final String TEST_ACTIVE = "Z";
+    private final String TOSTRING_FSTRING = "%.2fmg %s Pill";
+    private final String DESCRIPTION_FSTRING = "%s Pill\n\tStrength: %.2f\n\tSize: %.2f\n\tColor: %s\n\tCasing: %s\n\tSolution: %s\n\tActive: %s\n";
+    private final String MANUFACTURE_FSTRING = "\nManufacturing...\n%s\n%s\n%s\n... completed manufacturing.\n";
+    
+    private ByteArrayOutputStream baos;
+    private PrintStream oldOut;
+    private Dreamly obj;
+
+    private String getOutput() {
+        // flush all data from the PrintStream into our ByteArrayOutputStream
+        System.out.flush();	
+        // return the output with return characters stripped.
+        // stripping return characters ensure the test works on mac, unix, and windows
+        return baos.toString().replaceAll("\r", "");
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        // this.obj = new GelCapMock(TEST_NAME, TEST_STRENGTH, TEST_SIZE, TEST_COLOR);
+        
+        this.oldOut = System.out;
+        this.baos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(baos));
+        this.obj = new Dreamly(TEST_STRENGTH, TEST_SIZE, TEST_COLOR);
+    }
+    
+    @AfterEach
+    public void afterEach() {
+        System.setOut(oldOut);
+    }
+
+    @Test
+    public void testName() {
+        assertEquals(CORRECT_NAME, obj.getName());
+    }
+
+    @Test
+    public void testStrength() {
+        assertEquals(TEST_STRENGTH, obj.getStrength());
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(TEST_SIZE, obj.getSize());
+    }
+
+    @Test
+    public void testColor() {
+        assertEquals(TEST_COLOR, obj.getColor());
+    }
+
+    @Test
+    public void testCasing() {
+        assertEquals(TEST_CASING, obj.getCasing());
+    }
+
+    @Test
+    public void testSolution() {
+        assertEquals(TEST_SOLUTION, obj.getSolution());
+    }
+
+    @Test
+    public void testActive() {
+        assertEquals(TEST_ACTIVE, obj.getActive());
+    }
+
+    @Test
+    public void testManufactureProcess() {
+        assertEquals(String.format(MANUFACTURE_FSTRING, obj.getCasing(), obj.getSolution(), obj.getActive()), getOutput());
+    }
+
+
+}
