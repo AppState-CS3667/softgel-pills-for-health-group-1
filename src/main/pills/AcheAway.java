a7b7d7a844cae091d567ec3cd55b6adb82ae0212
ThAckerman <ackermanth@appstate.edu>
2023-02-24 14:12:44 -0500

    Finished Story4 - AcheAway subclass

--- src/main/pills/AcheAway.java
+++ src/main/pills/AcheAway.java
@@ -1,5 +1,27 @@
-package main.pills;
+package pills;
+import pills.GelCap;
 
 public class AcheAway extends GelCap{
+    public AcheAway(double strength, String color, double size ) {
+        super("AcheAway", strength, size, color);
+    }
+    
+    @Override
+    protected void addCasing() {
+        casing = "Gelatin";
+        System.out.printf("adding %s casing\n", casing);
+    }
+
+    @Override
+    protected void addSolution() {
+        solution = "Saline";
+        System.out.printf("adding %s solution\n", casing);
+    }
+
+    @Override
+    protected void addActive() {
+        active = "Acetaminophen";
+        System.out.printf("adding %s active\n", casing);
+    }
     
 }
