4e334a62d921d44208bbb943e84bbf2912fcc352
May Jiang <jiangl1@appstate.edu>
2023-03-20 14:25:50 -0400

    Created Adult and Child version of AcheAway

--- src/main/pills/ChildAcheAway.java
+++ src/main/pills/ChildAcheAway.java
@@ -1,13 +1,27 @@
 package pills;
-
 import pills.AcheAway;
 
+
+
+
+/**
+ * ChildAcheAway is a subclass of the AcheAway class.
+ * @author Subteam B
+ * @version 1.0
+ */
 public class ChildAcheAway extends AcheAway
 {
 	static final double STRENGTH = 415.00;
 	static final double SIZE = 3.25;
 	static final String COLOR = "cyan";
 	
+
+	/**
+	 * Constructor for ChildAcheAway
+	 * @param casing
+	 * @param solution
+	 * @param active
+	 */	
 	public ChildAcheAway(String casing, String solution, String active)
 	{
 		super(STRENGTH, SIZE, COLOR, casing, solution, active);
