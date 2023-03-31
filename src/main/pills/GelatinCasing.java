6f93ce7d54553d7c372d7320ac50c757d06a646a
axsomer <alxsmr99@gmail.com>
2023-03-31 12:08:28 -0400

    added GeltinCasing

---
+++ src/main/pills/GelatinCasing.java
@@ -0,0 +1,10 @@
+package pills;
+
+public class GelatinCasing implements CasingGenerator{
+
+    @Override
+    public String generateCasing(){
+        String output = String.format("Mixing gelatin, water, opacifier, and glycerin .\nShaping .\nReturning gelatin casing .\n");
+        System.out.print(output);
+        return "gelatin";
+}
