95c026e13ffed7c71340e390f368d52911c34eaa
ThAckerman <ackermanth@appstate.edu>
2023-04-01 15:10:21 -0400

    Added skeleton methods for SoftGelPillStore

---
+++ src/main/pills/SoftGelPillStore.java
@@ -0,0 +1,67 @@
+package pills;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SoftGelPillStore {
+    private GelCapFactory factory;
+    private ArrayList<GelCap> currentOrder;
+    private Scanner input;
+    private PrintStream output;
+    private String customerName;
+    private int customerAge;
+    private boolean isLggedIn;
+
+    public SoftGelPillStore(Scanner input, PrintStream output) {
+
+    }
+
+    public SoftGelPillStore(PrintStream output) {
+
+    }
+
+    public SoftGelPillStore(Scanner input) {
+
+    }
+
+    public SoftGelPillStore() {
+
+    }
+
+    public GelCap [] checkOut() {
+
+    }
+
+    public void order() {
+
+    }
+
+    public void logIn() {
+
+    }
+
+    public void logIn(String name, int age) {
+
+    }
+
+    public boolean logOut() {
+
+    }
+
+    public void setOutput(PrintStream output) {
+
+    }
+
+    public void setInput(Scanner input) {
+
+    }
+
+    public PrintStream getOutput() {
+
+    }
+
+    public Scanner getInput() {
+        
+    }
+}
