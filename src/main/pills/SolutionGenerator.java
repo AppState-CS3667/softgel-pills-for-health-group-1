11931bd94b4af8bfa40a550094cc4aae3c5f17a8
axsomer <alxsmr99@gmail.com>
2023-04-17 14:31:05 -0400

    generator changes

--- src/main/pills/SolutionGenerator.java
+++ src/main/pills/SolutionGenerator.java
@@ -1,11 +1,13 @@
 package pills;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  * This is the SolutionGenerator interface.
  * @author Subteam B
  * @version 1.0
  */
-public interface SolutionGenerator 
+public interface SolutionGenerator extends Remote
 {
-    public String generateSolution();
+    public String generateSolution() throws RemoteException;
 }
