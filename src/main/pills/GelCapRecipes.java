193d90f1634564978c2d8267336c7e4d3555fcdf
May Jiang <jiangl1@appstate.edu>
2023-04-17 15:09:56 -0400

    Initial commit of GelCapRecipes

--- src/main/pills/GelCapRecipes.java
+++ src/main/pills/GelCapRecipes.java
@@ -1,34 +1,77 @@
 package pills;
 
 import java.util.HashMap;
+import java.rmi.Remote;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.net.MalformedURLException;
 
-public class GelCapRecipes 
+import pills.ActiveGenerator;
+import pills.CasingGenerator;
+import pills.SolutionGenerator;
+
+/**
+ * GelCapRecipes that remotely access generators
+ *
+ * @author subteamA
+ * @version 04/10/2023
+ */
+
+public class GelCapRecipes extends UnicastRemoteObject implements Remote 
 {
     public static final HashMap<String, CasingGenerator> CASINGS = getCasings();
     public static final HashMap<String, SolutionGenerator> SOLUTIONS = getSolutions();
     public static final HashMap<String, ActiveGenerator> ACTIVES = getActives();
+    private static final String str = "rmi://152.10.10.40/";
+
+    
+	
 
     private static HashMap<String, CasingGenerator> getCasings()
     {
-        HashMap<String, CasingGenerator> thisMap = new HashMap<String, CasingGenerator>();
-        thisMap.put("dreamly", new GelatinCasing());
-        thisMap.put("acheAway", new PlasticizerCasing());
-        return thisMap;
+        try
+	{
+	HashMap<String, CasingGenerator> thisMap = new HashMap<String, CasingGenerator>();
+	CasingGenerator cd = (CasingGenerator) (Naming.lookup(addr + "Gelatin"));
+	CasingGenerator ca = (CasingGenerator) (Naming.lookup(addr + "Plasticizer"));
+        thisMap.put("dreamly", cd);
+        thisMap.put("acheAway", ca);
+        } catch (MalformedURLException | RemoteException e)
+	{
+		e.printStackTrace();	
+	}
+	return thisMap;
+	
     }
 
     private static HashMap<String, SolutionGenerator> getSolutions()
     {
-        HashMap<String, SolutionGenerator> thisMap = new HashMap<String, SolutionGenerator>();
-        thisMap.put("dreamly", new OilSolution());
-        thisMap.put("acheAway", new SalineSolution());
-        return thisMap;
-    }
+	try
+	{
+	HashMap<String, SolutionGenerator> thisMap = new HashMap<String, SolutionGenerator>();
+	SolutionGenerator sd = (SolutionGenerator) (Naming.lookup(addr + "Oil"));
+	SolutionGenerator sa = (SolutionGenerator) (Naming.lookup(addr + "Saline"));
+	thisMap.put("dreamly", sd);
+	thisMap.put("acheAway", sa);
+	} catch (MalformedURLException | RemoteException e)
+	{
+		e.printStackTrace();
+	}
+	return thisMap; 
+   }
 
-    private static HashMap<String, ActiveGenerator> getActives()
-    {
-        HashMap<String, ActiveGenerator> thisMap = new HashMap<String, ActiveGenerator>();
-        thisMap.put("dreamly", new AcetaminophenActive());
-        thisMap.put("acheAway", new ZolpidemActive());
-        return thisMap;
-    }
+   private static HashMap<String, ActiveGenerator> getGenerator()
+   {
+	try
+	{
+	HashMap<String, ActiveGenerator> thisMap = new HashMap<String, ActiveGenerator>();
+	ActiveGenerator ad = (ActiveGenerator) (Naming.lookup(addr + "Acetaminophen"));
+	ActiveGenerator aa = (ActiveGenerator) (Naming.lookup(addr + "Zolpidem"));
+	} catch (MalformedURLException | RemoteException e)
+	{
+		e.printStackTrace();
+	}
+	return thisMap;
+   }
 }
+
