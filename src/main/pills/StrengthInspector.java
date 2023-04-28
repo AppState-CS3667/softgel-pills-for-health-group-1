602a5967016c1ed541f815f5fdbf44248aea8d92
patriciagarcia654 <izquierdogarciap@appstate.edu>
2023-04-28 12:41:33 -0400

    Created StrengthInspector

---
+++ src/main/pills/StrengthInspector.java
@@ -0,0 +1,35 @@
+package pills;
+public class StrengthInspector extends Inspector{
+    private double dreamlyStrength;
+    private double acheAwayStrength;
+    public double getDreamlyStrength(){
+        return dreamlyStrength;
+    }
+    public double getAcheAwayStrength(){
+        return acheAwayStrength;
+    }
+    @Override
+    public void inspect(AdultAcheAway e){
+        acheAwayStrength += e.getStrength(); 
+    }
+    @Override
+    public void inspect(ChildAcheAway e){
+        acheAwayStrength += e.getStrength();
+    }
+    @Override
+    public void inspect(AdultDreamly e){
+   		dreamlyStrength += e.getStrength(); 
+	 }
+    @Override
+    public void inspect(ChildDreamly e){
+        dreamlyStrength += e.getStrength(); 
+    }
+    public String report(){
+		return "Strength Report\n\tAcheAway Pills : " + getAcheAwayStrength() + "\n\tChild Pills : " + getDreamlyStrength(); 
+	}
+	 }
+    public void reset(){
+        this.acheAwayStrength = 0;
+        this.dreamlyStrength = 0;
+    }
+}
